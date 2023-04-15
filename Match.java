7faf152552f801acb151c5b70895f00657583e11
M7Saad <84126236+M7Saad@users.noreply.github.com>
2023-04-15 06:37:32 +0300

    Added more attributes and constructors and setter and getters

--- Match.java
+++ Match.java
@@ -7,14 +7,27 @@ public class Match {
     private int goals1;
     private int goals2;
     private Date date;
+
+    //Match constructor
     public Match(Tournament t, Team a, Team b, Date d){
         tournament  = t;
         team1 = a;
         team2 = b;
         date = d;
     }
+    //another constructors with 1/without Teams
+    public Match(Tournament t, Date d, Team a){
+        tournament  = t;
+        date = d;
+        team1 = a;
+    }
+    public Match(Tournament t, Date d){
+        tournament  = t;
+        date = d;
+    }
 
 
+    //Getters
     public Date getDate() {
         return date;
     }
@@ -33,8 +46,17 @@ public class Match {
         return tournament;
     }
 
-
-
+    //Setters
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+    
 
     public void recordScore(int a,int b){
         goals1 = a; goals2 = b;
