bbd3431f882507e0dc20200d9cf9a2e7d883c3ec
M7Saad <84126236+M7Saad@users.noreply.github.com>
2023-04-15 06:40:54 +0300

    Added constructor

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 public class RoundRobin extends Tournament{
 
+    
+    // constructor
+    public RoundRobin(String name, boolean isIndividual, String sport, Date startDate) {
+        super(name, isIndividual, sport, startDate);
+    }
     
     public void generateMatches(int restDays) {
         
