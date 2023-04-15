abe50530047f6bceed8ad85dd3fe5a8481a74867
M7Saad <84126236+M7Saad@users.noreply.github.com>
2023-04-15 06:31:48 +0300

    Added more attributes and constructors and completed some method

--- Tournament.java
+++ Tournament.java
@@ -2,13 +2,27 @@ import java.util.Date;
 import java.util.ArrayList;
 
 abstract public class Tournament {
+    // attributes
     private String name;
     private boolean isIndividual;
     private String sport;
     private ArrayList<Team> teams;
     private Date startDate;
     private Date endDate;
+    private boolean hasFinished;
 
+    // constructor
+    public Tournament(String name, boolean isIndividual, String sport, Date startDate) {
+        this.name = name;
+        this.isIndividual = isIndividual;
+        this.sport = sport;
+        this.startDate = startDate;
+        //this.endDate = endDate;
+        this.teams = new ArrayList<Team>();
+        this.hasFinished = false;
+    }
+
+    // Getters 
     public boolean getIsIndividual(){
         return isIndividual;
     }
@@ -27,12 +41,20 @@ abstract public class Tournament {
     public ArrayList<Team> getTeams() {
         return teams;
     }
-
-    public abstract void generateMatches(int restDays);
-    public abstract void viewStanding();
-    public void addTeam(Team t){
-        // not done yet
+    public int getNumberOfTeams() {
+        return teams.size();
+    }
+    public boolean getHasFinished() {
+        return hasFinished;
     }
 
+    // abstract methods
+    public abstract void generateMatches(int restDays) throws Exception;
+    public abstract void viewStanding();
+
+    // non-abstract methods
+    public void addTeam(Team t){
+        teams.add(t);
+    }
 
 }
