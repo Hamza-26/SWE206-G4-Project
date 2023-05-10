0be921c209b0f660a4992c62ca693e716c73dc84
Hamza-26 <125142557+Hamza-26@users.noreply.github.com>
2023-05-10 17:07:15 +0300

    added some unnecessary stuff

---
+++ Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+
+    
+    public void start(Stage arg0) throws Exception {
+
+        
+    }
+    
+}
